339e5956827752a50d2820544b7e3bbf2435d546
zhainaipeng <zhainaipeng@gmail.com>
2015-12-02 11:26:24 +0800

    test version

--- src/main/java/com/smartdevice/controller/SmartCarController.java
+++ src/main/java/com/smartdevice/controller/SmartCarController.java
@@ -1,9 +1,12 @@
 package com.smartdevice.controller;
 
+import org.apache.logging.log4j.core.appender.SyslogAppender;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SmartCarController {
-
 	
+	public static void main(String[] args){
+		System.out.println("test");
+	}
 }
